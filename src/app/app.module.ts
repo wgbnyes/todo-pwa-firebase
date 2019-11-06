7018dc1d9f68a41068d8dfd5603e5442b572edae
Walter Gandarella <waltergandarella@yesmkt.com>
2019-11-06 11:49:41 +0000

    configure firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        AngularFireModule.initializeApp({
+            apiKey: "AIzaSyDuCmoMNLzm3p0xiLHwA63FFYSq6sibFxc",
+            authDomain: "yes4develop.firebaseapp.com",
+            databaseURL: "https://yes4develop.firebaseio.com",
+            projectId: "yes4develop",
+            storageBucket: "yes4develop.appspot.com",
+            messagingSenderId: "822997869888",
+            appId: "1:822997869888:web:35b8fd1538ffe1c79246a5"
+        }),
+        AngularFirestoreModule.enablePersistence(),
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
